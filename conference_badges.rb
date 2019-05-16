a969151d71bf1316f5ef588e7fd3943b45d816fb
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-16 20:10:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(attendees)
   b = attendees.each do |name|
     puts name
   end
-  "Hello, my name is  #{attendees[i]}"
+  "Hello, my name is  #{attendees[name]}."
 end
 
 def assign_rooms()
