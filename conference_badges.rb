53f8ae2c5a1e5f4108680023f8ae22d09117b0ef
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-16 19:50:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator(attendees)
   i = 0
   while i < attendees.length
-    return "Hello, my name is #{attendees[i]}."
+    puts "Hello, my name is #{attendees[i]}."
     i += 1
   end
 end
