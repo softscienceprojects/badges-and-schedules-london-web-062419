5e5bc03d2fdae4f38e3e06b6c6fbda3b40358881
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-16 19:32:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker(name)
+  greet = "Hello, my name is #{name}."
+  greet
+end
+
+def batch_badge_creator(attendees)
+  i = 0
+  loop do
+    puts attendees[i]
+    i += 1
+  end
+end
+
+def assign_rooms()
+
+end
+
+def printer
+
+end
